49a88af41326dacc9e4357023026af264e4c8633
Civitasv <hscivitasv@gmail.com>
2022-10-26 19:41:07 +0800

    Move json_element implementation to header

--- include/json_element.h
+++ include/json_element.h
@@ -37,7 +37,33 @@ class JsonElement {
   };
 
   JsonElement() : type_(Type::JSON_NULL) {}
-  JsonElement(Type type);
+
+  JsonElement(Type type) : type_(type) {
+    switch (type) {
+      case Type::JSON_OBJECT:
+        value_.value_object = new std::map<std::string, JsonElement*>();
+        break;
+      case Type::JSON_ARRAY:
+        value_.value_array = new std::vector<JsonElement*>();
+        break;
+      case Type::JSON_STRING:
+        value_.value_string = new std::string("");
+        break;
+      case Type::JSON_NUMBER:
+        value_.value_number = 0;
+        break;
+      case Type::JSON_TRUE:
+        value_.value_bool = true;
+        break;
+      case Type::JSON_FALSE:
+        value_.value_bool = false;
+        break;
+      case Type::JSON_NULL:
+        break;
+      default:
+        break;
+    }
+  };
 
   ~JsonElement() {
     if (type_ == Type::JSON_OBJECT) {

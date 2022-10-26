9cedc6d47e6bb87d4f6c8d9aca975141819cd61d
Civitasv <hscivitasv@gmail.com>
2022-10-26 12:35:39 +0800

    init library structure

---
+++ src/parser.cc
@@ -0,0 +1,7 @@
+#include "include/parser.h"
+
+namespace civitasv {
+namespace json {
+class Parser {};
+}  // namespace json
+}  // namespace civitasv
---
+++ src/scanner.cc
@@ -0,0 +1,7 @@
+#include "include/scanner.h"
+
+namespace civitasv {
+namespace json {
+class Scanner {};
+}  // namespace json
+}  // namespace civitasv
